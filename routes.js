049e056cfd5d633d8730833d386fb95dfb257241
HazelMpR <hazelmpr368@gmail.com>
2024-06-16 19:41:01 +0700

    hapi server

---
+++ routes.js
@@ -0,0 +1,32 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tsb';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Page not found';
+        },
+    },
+];
+
+module.exports = routes;
